f43b02cdcfa83a7cf886a7e43a6af7d1dc415756
Lorenzo Marzullo <marzullo.lorenzo@gmail.com>
2016-09-27 20:08:00 +0200

    Added json strategy

---
+++ src/Strategy/JsonStrategy.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of the ZendHydratorUtilities package.
+ *
+ * (c) Lorenzo Marzullo <marzullo.lorenzo@gmail.com>
+ */
+
+namespace ZendHydratorUtilities\Strategy;
+
+use Zend\Hydrator\Strategy\StrategyInterface;
+
+/**
+ * Class JsonStrategy.
+ *
+ * @package ZendHydratorUtilities
+ * @package Lorenzo Marzullo <marzullo.lorenzo@gmail.com>
+ * @package https://github.com/lorenzomar/zend-hydrator-utilities
+ */
+class JsonStrategy implements StrategyInterface
+{
+    /**
+     * @var bool
+     */
+    private $decodeAsAssoc;
+
+    public function __construct($decodeAsAssoc = true)
+    {
+        $this->decodeAsAssoc = (bool) $decodeAsAssoc;
+    }
+
+    public function extract($value)
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        return json_encode($value);
+    }
+    
+    public function hydrate($value)
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        json_decode($value, $this->decodeAsAssoc);
+    }
+}
